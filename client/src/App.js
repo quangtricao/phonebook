088c92c1b1cdcf33636d2cb59eecb39c4ecba275
Tri Cao <tricq@flextech.vn>
2023-02-25 12:05:21 +0200

    convert indentation to spaces

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,11 @@ import Notification from "./components/Notification";
 import personService from "./services/persons";
 
 const App = () => {
-	const [persons, setPersons] = useState([]);
-	const [newName, setNewName] = useState("");
-	const [newNumber, setNewNumber] = useState("");
-	const [filter, setFilter] = useState("");
-	const [notification, setNotification] = useState(null);
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const [notification, setNotification] = useState(null);
 
   // After the first render, fetch data from the server to render again but only once throughout the App component lifetime.
   useEffect(() => {
@@ -19,32 +19,32 @@ const App = () => {
   }, []);
 
   // set Notification with default info type
-	const notify = (message, type = "info") => {
-		setNotification({ message, type });
+  const notify = (message, type = "info") => {
+    setNotification({ message, type });
 
-		// set back to null after 3 seconds to make the noti disappear
-		setTimeout(() => {
-			setNotification(null);
-		}, 3000);
-	};
+    // set back to null after 3 seconds to make the noti disappear
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000);
+  };
 
-	const addPerson = (event) => {
-		event.preventDefault(); // prevents the default behaviour of the form element
+  const addPerson = (event) => {
+    event.preventDefault(); // prevents the default behaviour of the form element
 
-		const newPerson = {
-			name: newName,
-			number: newNumber,
-		};
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+    };
 
-		setNewName("");
-		setNewNumber("");
+    setNewName("");
+    setNewNumber("");
 
-		const existingPerson = persons.find((person) => person.name === newPerson.name);
+    const existingPerson = persons.find((person) => person.name === newPerson.name);
 
     // if newPerson's name already exists, update his/her number
     // else add newPerson to the persons state.
-		if (existingPerson) {
-			if (window.confirm(`${newPerson.name} is already added to phonebook, update the number?`)) {
+    if (existingPerson) {
+      if (window.confirm(`${newPerson.name} is already added to phonebook, update the number?`)) {
         personService
           .update(existingPerson.id, {
             ...existingPerson,
@@ -59,8 +59,8 @@ const App = () => {
           .catch((error) => {
             notify(error.response.data.error, "error");
           });
-			} else return;
-		} else {
+      } else return;
+    } else {
       personService
         .create(newPerson)
         .then((savedPerson) => {
@@ -76,48 +76,48 @@ const App = () => {
   const deletePerson = (id) => {
     const toDelete = persons.find((p) => p.id === id);
 
-		if (window.confirm(`Delete ${toDelete.name}`)) {
-			personService
-				.remove(id)
-				.then(() => {
-					setPersons(persons.filter((p) => p.id !== id));
-					notify(`Deleted ${toDelete.name}`);
-				})
-				.catch((error) => {
-					notify(error.response.data.error, "error");
-					setPersons(persons.filter((p) => p.id !== toDelete.id));
-				});
-		}
-	};
+    if (window.confirm(`Delete ${toDelete.name}`)) {
+      personService
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter((p) => p.id !== id));
+          notify(`Deleted ${toDelete.name}`);
+        })
+        .catch((error) => {
+          notify(error.response.data.error, "error");
+          setPersons(persons.filter((p) => p.id !== toDelete.id));
+        });
+    }
+  };
 
-	// filter persons by name. If there is no letter to filter, return the whole list
-	// else return the list that contains the letter
-	const personsToShow = filter.length === 0
-			? persons
-			: persons.filter((p) => p.name.toLowerCase().includes(filter.toLowerCase()));
+  // filter persons by name. If there is no letter to filter, return the whole list
+  // else return the list that contains the letter
+  const personsToShow = filter.length === 0
+      ? persons
+      : persons.filter((p) => p.name.toLowerCase().includes(filter.toLowerCase()));
 
-	return (
-		<div>
-			<h2>Phonebook</h2>
-			<Notification notification={notification} />
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification notification={notification} />
 
-			<div>
-				Name filter{" "}
-				<input value={filter} onChange={(event) => setFilter(event.target.value)} />
-			</div>
-			<br />
+      <div>
+        Name filter{" "}
+        <input value={filter} onChange={(event) => setFilter(event.target.value)} />
+      </div>
+      <br />
 
-			<PersonForm
-				name={newName}
-				number={newNumber}
-				handleNameChange={(event) => setNewName(event.target.value)}
-				handleNumberChange={(event) => setNewNumber(event.target.value)}
-				addPerson={addPerson}
-			/>
+      <PersonForm
+        name={newName}
+        number={newNumber}
+        handleNameChange={(event) => setNewName(event.target.value)}
+        handleNumberChange={(event) => setNewNumber(event.target.value)}
+        addPerson={addPerson}
+      />
 
-			<Persons persons={personsToShow} handleDelete={deletePerson}/>
-		</div>
-	);
+      <Persons persons={personsToShow} handleDelete={deletePerson}/>
+    </div>
+  );
 };
 
 export default App;
--- client/src/components/Persons.js
+++ client/src/components/Persons.js
@@ -1,18 +1,18 @@
 const Persons = ({ persons, handleDelete }) => {
-	return (
-		<>
-			<h2>Numbers</h2>
+  return (
+    <>
+      <h2>Numbers</h2>
 
-			{persons.map((person) => (
-				<div key={person.name}>
-					{person.name} {person.number}{" "}
-					<button onClick={() => handleDelete(person.id)}>
-						delete
-					</button>
-				</div>
-			))}
-		</>
-	);
+      {persons.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}{" "}
+          <button onClick={() => handleDelete(person.id)}>
+            delete
+          </button>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Persons;
