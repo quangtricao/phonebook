28fca28e245d1ecd86345954cf33aaeb5af65cd1
Tri Cao <tricq@flextech.vn>
2023-02-25 19:43:45 +0200

    Add validations

--- server/models/person.js
+++ server/models/person.js
@@ -18,8 +18,27 @@ mongoose
 	});
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
+	name: {
+		type: String,
+		minLength: 3,
+		required: [true, "name is missing"],
+	},
+
+	// A phone number must be formed of two parts that are separated by -
+	// the first part must only have two or three numbers
+	// the second part also consists of numbers that must has length of 6 or more
+	// eg. 09-1234556 and 040-22334455              are valid
+	// eg. 1234556, 1-22334455 and 10-22-334455     are invalid
+	number: {
+		type: String,
+		required: [true, "phone number is missing"],
+		validate: {
+			validator: function (v) {
+				return /\d{2,3}-\d{6,}/.test(v);
+			},
+			message: (props) => `${props.value} is not a valid phone number!`,
+		},
+	},
 });
 
 // Modify the toJSON() method of the schema to format the objects returned by Mongoose,
